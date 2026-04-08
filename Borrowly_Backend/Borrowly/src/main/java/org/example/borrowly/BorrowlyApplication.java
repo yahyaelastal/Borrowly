200af144947a7ec4a7fed23ea61abb63cfaeb9fe
yahyaelastal <yahia.elastal@stud.fils.upb.ro>
2026-04-08 19:23:26 +0300

    Spring Boot Backend

---
+++ Borrowly_Backend/Borrowly/src/main/java/org/example/borrowly/BorrowlyApplication.java
@@ -0,0 +1,13 @@
+package org.example.borrowly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowlyApplication.class, args);
+	}
+
+}
